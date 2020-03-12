35d36d8641e79c6f199d23e16a7f2b6aee38789d
Brais Cidras <braisomary@gmail.com>
2020-03-12 10:15:02 +0200

    Create OnClickListener

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,6 +17,9 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener {
+
+        }
         return binding.root
     }
 
