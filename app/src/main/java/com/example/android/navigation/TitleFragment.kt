fcce5d38dff3f2ead76838c6030123a8d4fc9805
Brais Cidras <braisomary@gmail.com>
2020-03-12 10:20:27 +0200

    Navigating with our action

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -19,7 +19,7 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener {
-            view: View -> Navigation.findNavController(view)
+            view: View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
