707493191bee8d44c107ccf3538a0639df9f4fda
Brais Cidras <braisomary@gmail.com>
2020-03-13 13:56:01 +0200

    Fragment titles fixed

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -3,7 +3,6 @@ package com.example.android.navigation
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
